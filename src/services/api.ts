935c93c86b72ae141fd5ea34995d9f081c6193f3
Plantao Aqui <aqui.plantao@gmail.com>
2021-01-21 15:01:10 -0300

    Correção de ip

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api =  axios.create({
-    baseURL: 'ip-172-31-11-200.sa-east-1.compute.internal/',
+    baseURL: 'https://ip-172-31-11-200.sa-east-1.compute.internal/',
 });
 
 export default api;
